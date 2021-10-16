606cda9601a49a647e81b0710777e80b48f0a357
Clausemberg <clausemberg@yahoo.com.br>
2021-10-16 13:11:43 -0300

    Aula1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,35 @@
-import type { NextPage } from 'next'
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle'
-
+import type { NextPage } from "next";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle title="Conheça os Profissionais"
+      <PageTitle
+        title="Conheça os Profissionais"
         subtitle="Preencha seu endereço e veja todos os profissionais de sua cidade"
-      >
-      asdfsadf
-        
-      </PageTitle>
+      ></PageTitle>
+      <UserInformation
+        name="Clausemberg"
+        picture="https://avatars.githubusercontent.com/u/50032538?v=4"
+        rating={3}
+        description="São Mateus-ES"
+      />
+      <UserInformation
+        name="Clausemberg"
+        picture="https://avatars.githubusercontent.com/u/50032538?v=4"
+        rating={3}
+        description="São Mateus-ES"
+      />
+      <UserInformation
+        name="Clausemberg"
+        picture="https://avatars.githubusercontent.com/u/50032538?v=4"
+        rating={3}
+        description="São Mateus-ES"
+      />
     </div>
-  )
+  );
+};
 
-}
-
-export default Home
+export default Home;
